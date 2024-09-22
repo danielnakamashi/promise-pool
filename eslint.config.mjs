38a7466bed29161de47598538f0e7f758d18bb78
Daniel Nakamashi <daniel.nakamashi@auto1.com>
2024-09-22 22:57:44 +0200

    feat: add initial config

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+const typescriptFiles = ['**/*.ts']
+
+export default tseslint.config(
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked.map((config) => ({
+    ...config,
+    files: typescriptFiles,
+  })),
+  ...tseslint.configs.stylisticTypeChecked.map((config) => ({
+    ...config,
+    files: typescriptFiles,
+  })),
+  {
+    files: typescriptFiles,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+)
